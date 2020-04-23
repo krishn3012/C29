13e134c5790d0e8bab154044f94e1cca17244f8e
krishn3012 <krishngupta3012@gmail.com>
2020-04-23 20:05:31 +0530

    added the catapult and rubber band pics

--- Slingshot.js
+++ Slingshot.js
@@ -9,6 +9,10 @@ class SlingShot{
         this.pointB = pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
+
+        this.catapult1= loadImage("sprites/sling1.png");
+        this.catapult2= loadImage("sprites/sling2.png");
+        this.rubberband= loadImage("sprites/sling3.png");
     }
 
     fly(){
@@ -17,11 +21,36 @@ class SlingShot{
 
     display(){
         if(this.sling.bodyA){
+            
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            
+            push();
+
+            if (pointA.x>220){
+                strokeWeight(3);
+                stroke(89, 26, 26);
+                line(pointA.x+20, pointA.y, pointB.x-20, pointB.y-40);
+                line(pointA.x+20, pointA.y, pointB.x+20, pointB.y-40);
+                image(this.rubberband,pointA.x+20,pointA.y-20,15,30);
+            }
+            else{
+
+                strokeWeight(8);
+                stroke(35, 12, 12);
+                line(pointA.x-20, pointA.y, pointB.x-20, pointB.y-40);
+                line(pointA.x-20, pointA.y, pointB.x+20, pointB.y-40);
+                image(this.rubberband,pointA.x-30,pointA.y-20,15,30);
+            }
+            pop();
         }
+
+        image(this.catapult1,200,20);
+        image(this.catapult2,170,20);
+        
+        
+
+
     }
     
 }
